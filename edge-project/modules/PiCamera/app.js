bac1ef21fa5292c72e358a812531d8d975d5ed07
drcrook1 <drcrook@outlook.com>
2018-01-15 17:12:42 -0500

    Working Edge Module posts csv string of pixel values in correct flattened format

--- edge-project/modules/PiCamera/app.js
+++ edge-project/modules/PiCamera/app.js
@@ -8,25 +8,56 @@ var Message = require('azure-iot-device').Message;
 const Raspistill = require('node-raspistill').Raspistill;
 const pixel_getter = require('pixel-getter');
 
+const width = 50;
+const height = 50;
+
 // fromConnectionString must specify a transport constructor, coming from any transport package.
 var client = Client.fromConnectionString(connectionString, Protocol);
 
 const camera = new Raspistill({ verticalFlip: true,
   horizontalFlip: true,
-  width: 50,
-  height: 50,
+  width: width,
+  height: height,
   encoding: 'jpg',
   noFileSave: true,
   time: 5 });
+
+// function PixelsToArray(pixels) {
+//   var arr_pixels = '';
+//   console.log(pixels);
+//   for(var i =0; i < width; i++){
+//     for(var j=0; j < height; j++){
+//       p = pixels[i][j];
+//       arr_pixels += String(p.r) + ',';
+//       arr_pixels += String(p.g) + ',';
+//       arr_pixels += String(p.b) + ',';
+//     }
+//   }
+//   arr_pixels = arr_pixels.slice(0, -1); //Slice off last comma.
+//   return arr_pixels;
+// }
+
+function PixelsToArray(pixels){
+  var arr_pixels = '';
+  Object.keys(pixels).forEach(i => {
+    Object.keys(pixels[i]).forEach(j => {
+      arr_pixels += pixels[i][j].r + ',';
+      arr_pixels += pixels[i][j].g + ',';
+      arr_pixels += pixels[i][j].b + ',';
+    });
+  });
+  return arr_pixels.slice(0, -1); //slice off last comma.
+}
 
 function TakePictureLoop() {
   console.log('taking picture');
   camera.takePhoto().then((photo) => {
       console.log('got photo');
       pixel_getter.get(photo, function(err, pixels) {
-          console.log('got pixels');
-          console.log(pixels)
-          var data = JSON.stringify({ deviceId: 'NODEJS_RPI', image: 'photo_received' });
+          console.log('got pixels');         
+          var pixel_arr = PixelsToArray(pixels);
+          console.log(pixel_arr);
+          var data = JSON.stringify({ deviceId: 'NODEJS_RPI', image: pixel_arr });
           var message = new Message(data);   
           client.sendEvent(message, printResultFor('send'));  
           TakePictureLoop();
--- edge-project/modules/PiCamera/scripts/publishimage.ps1
+++ edge-project/modules/PiCamera/scripts/publishimage.ps1
@@ -1,3 +1,3 @@
 
-docker build --pull -t iotml.azurecr.io/picamera:0.13 .
-docker push iotml.azurecr.io/picamera:0.13
+docker build --pull -t iotml.azurecr.io/picamera:0.20 .
+docker push iotml.azurecr.io/picamera:0.20
